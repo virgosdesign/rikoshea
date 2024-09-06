d22d7fcc232df0c39ad0ad525719bae00d22f706
K Robinson <kthrob@Ks-Mac-mini.local>
2024-09-06 08:58:43 -0700

    Change output in config to server.

--- astro.config.mjs
+++ astro.config.mjs
@@ -131,7 +131,7 @@ export default defineConfig({
     gzip: false,
     brotli: true
   }), icon()],
-  output: "hybrid",
+  output: "server",
   experimental: {
     clientPrerender: true,
     directRenderScript: true
